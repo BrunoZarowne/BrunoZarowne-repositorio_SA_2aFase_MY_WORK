fa4bfe9db1ab8e4a5d94f87bba76be714289297e
caiolorramvalente <caio_valente@estudante.sesisenai.org.br>
2024-10-01 17:23:32 -0300

    Realização da landing page do my work

---
+++ MyWork_codigos/src/pages/LandingPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './LandingPage.css'
+function LandingPage() {
+  return (
+    <div className='containerLanding'>
+      <div></div>
+
+      <div></div>
+
+      <div className='divText'>
+        <h1 className='h1Landing'>Free your <br></br>
+            imagination</h1>
+            <div className='divTextP'>
+            <p className='pInfoSite'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />  Quam a ab est deleniti quod, quas libero maxime dolorum! <br /> Corrupti <br /> quasi blanditiis quaerat cupiditate nobis dolor enim, nostrum culpa veritatis <br /> reiciendis!</p>
+            </div>
+            <div className='divButaos'>
+                <button className='butaoUm'>Dowload</button> <button className='butaoDois'>Sing</button>
+            </div>
+        </div>
+    
+    </div>
+  )
+}
+
+export default LandingPage
