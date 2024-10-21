77fc906c27a57e6bbab545cd03a2b02398b2d9f2
vithorLorramValente7 <vithor_valente@estudante.sesisenai.org.br>
2024-10-21 00:05:19 -0300

    tela de login my work

--- MyWork_codigos/src/pages/Login.jsx
+++ MyWork_codigos/src/pages/Login.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import './login.css'
+import { Link } from 'react-router-dom'
+
 function Login() {
+ 
+ 
   return (
     <div className='divImagemFundo'>
      <div className='divContainerInfo'>
-        <h1>login</h1>
+      
+        <p className='login_h1'>LOGIN</p>
 
-        <p className='pText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eius neque <br /> ducimus dicta facilis unde cum. Ex harum, mollitia culpa officiis quaerat, unde impedit atque <br />doloribus alias iste necessitatibus sint?</p>
+        
+      <p className='pText'>"Bem-vindo ao nosso espaço literário! Faça login e mergulhe em um mundo repleto de histórias fascinantes,<br/> descobertas e conhecimento compartilhado. Junte-se a nós na celebração<br/> da literatura!.</p>
+   
+        
         <div className='divInpt'>
-            <input type="date" className='nomeUsuarioLogin' /> <br />
-            <input type="password" className='senhaUsuarioLogin' />
+
+
+            <input type="email" className='Email_User' placeholder='   insira seu email' /> <br />
+            <input type="password" className='senha_User'  placeholder='  insira sua senha'/>
+        
+       <div className='container_button'>
+        <button className='button_logar'>logar</button>
+        </div>
+        
+        <p className='naoPossuiUmaConta'>nao possui uma conta? clique <Link to={"/cadastro"} className='link_login'>Aqui</Link> </p>
+        
         </div>
 
 
