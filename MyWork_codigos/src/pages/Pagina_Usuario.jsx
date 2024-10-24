0225bab4abdbb02ca859f4a30fc4fb67e28bb99c
VITHOR LORRAM VALENTE <vithor_valente@estudante.sesisenai.org.br>
2024-10-24 17:19:23 -0300

    tela de usuario em produção

--- MyWork_codigos/src/pages/Pagina_Usuario.jsx
+++ MyWork_codigos/src/pages/Pagina_Usuario.jsx
@@ -1,13 +1,49 @@
 import React, { useState } from 'react'
 import './Pagina_Usuario.css'
+import { Link } from 'react-router-dom'
+
 function Pagina_Usuario() {
 const [fotoUsuario,setFotoUsuario]=useState('../images/Monstro_Vermelho.svg')
 
   return (
-    <div>
-       <div><img src={fotoUsuario} alt="" onClick={()=>{setFotoUsuario('../images/logo_sapo.png')}} /></div>
-        
-      
+    <div className='container_pag_usuario'>
+   
+     <div className='back_groud_user_profile_div'>
+     <Link>
+     
+     <img src="" alt="" />
+     
+     
+     </Link>
+  
+  
+  
+  <img src="./images/imagem_padrao_usuario.svg" alt="" />
+      </div>
+      <div className='div_container_informações_usuarios'>
+
+
+
+<div className='container_button'>
+<button className='button_doações'>doações</button>
+<button className='button_favoritos'>favoritos</button>
+<button className='button_historico'>historico</button>
+<button className='button_info_conta'>infromações da conta</button>
+</div>
+
+
+
+
+
+
+
+
+      </div>
+
+
+
+
+
     </div>
   )
 }
